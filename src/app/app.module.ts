0e8bf2ee879afda36f0b4e130b35e8b967ed5fef
Colin Worf and Mikey Pinaud <mikey@>
2018-01-24 11:49:38 -0800

    initialize all directories and installs, create components for layout and create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { RemovePersonComponent } from './remove-person/remove-person.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { EditProjectComponent } from './edit-project/edit-project.component';
+import { ProjectIdeasComponent } from './project-ideas/project-ideas.component';
+import { CompletedComponent } from './completed/completed.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProjectListComponent,
+    ProjectDetailComponent,
+    AddPersonComponent,
+    RemovePersonComponent,
+    AddProjectComponent,
+    EditProjectComponent,
+    ProjectIdeasComponent,
+    CompletedComponent
   ],
   imports: [
     BrowserModule
